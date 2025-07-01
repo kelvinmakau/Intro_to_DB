7468d6b7ef526a1d03f5cb768f152042637b404d
Kelvin Makau <kelvinmakau76@gmail.com>
2025-07-01 20:09:36 +0300

    SQL script

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,12 +24,15 @@ address TEXT
 CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
 customer_id INT,
-order_date DATE
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
 orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
-quantity DOUBLE
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
